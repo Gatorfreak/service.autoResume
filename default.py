eaa9428375087f4b14b31f9798c0bc19e3d413a9
Jeremy C <github@greenoyster.com>
2014-07-15 10:33:32 -0400

    Updated for XBMC 13 Gotham
    Minor updates to make it work with XBMC 13 Gotham and also to handle
    some timing issues with booting up and waiting for video to start before
    skipping ahead to where left off.

--- default.py
+++ default.py
@@ -21,21 +21,34 @@ from time import sleep
 
 ADDON = xbmcaddon.Addon()
 ADDON_ID = ADDON.getAddonInfo('id')
-FOLDER = ADDON.getSetting('autoresume.save.folder')
+FOLDER = ADDON.getSetting('autoresume.save.folder').encode('utf-8', 'ignore')
 FREQUENCY = int(ADDON.getSetting('autoresume.frequency'))
-PATH = os.path.join(FOLDER.encode('utf-8', 'ignore'), 'autoresume.txt')
+PATH = os.path.join(FOLDER, 'autoresume.txt')
 
 def resume():
-  if os.path.exists(PATH):
-    # Read from autoresume.txt.
-    f = open(PATH, 'r')
-    mediaFile = f.readline().rstrip('\n')
-    position = float(f.readline())
-    f.close()
-    # Play file.
-    xbmc.Player().play(mediaFile)
-    # Seek to last recorded position.
-    xbmc.Player().seekTime(position)
+  for x in range(0,120):
+    if os.path.exists(FOLDER):
+      if os.path.exists(PATH):
+        # Read from autoresume.txt.
+        f = open(PATH, 'r')
+        mediaFile = f.readline().rstrip('\n')
+        position = float(f.readline())
+        f.close()
+        # Play file.
+        xbmc.Player().play(mediaFile)
+        while (not xbmc.Player().isPlaying()):
+          sleep(0.5)
+        sleep(1)
+        # Seek to last recorded position.
+        xbmc.Player().seekTime(position)
+        sleep(1)
+        # Make sure it actually got there.
+        if abs(position - xbmc.Player().getTime()) > 30:
+          xbmc.Player().seekTime(position)
+      break
+    else:
+      # If the folder didn't exist maybe we need to wait longer for the drive to be mounted.
+      sleep(5)
 
 def recordPosition():
   if xbmc.Player().isPlaying():
